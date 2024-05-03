55c8cb9c2cd68bebc8dfebc8ab44eebc923ed36f
alyxandre-beniahia <alyxandre.beniahia@laplateforme.io>
2024-05-03 09:20:23 +0200

    added sparkling

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
-import React,{useState} from 'react'
-import './App.css'
-import Board from "./components/Board"
-import Title from './components/Title'
-import Header from './components/Header'
+import React, { useState, useEffect } from 'react';
+import Confetti from "react-confetti";
+import './App.css';
+import Board from "./components/Board";
+import Title from './components/Title';
+import Header from './components/Header';
 
 function App() {
-
   const [score, setScore] = useState(0);
   const [attempts, setAttempts] = useState(0);
+  const [showConfetti, setShowConfetti] = useState(false);
+
+  useEffect(() => {
+    if (score === 4) {
+      setShowConfetti(true);
+    } else {
+      setShowConfetti(false); // Reset showConfetti to false when score is not 4
+    }
+
+    return () => {
+      setShowConfetti(false); // Cleanup: set showConfetti to false when component unmounts
+    };
+  }, [score]);
+
   return (
     <>
-    <Title content="Memory Game"/>
-    <Header score={score} attempts={attempts}/>
-     <Board setScore={setScore} setAttempts={setAttempts}/>
+      <Title content="Memory Game"/>
+      <Header score={score} attempts={attempts}/>
+      {showConfetti && <Confetti />}
+      <Board setScore={setScore} setAttempts={setAttempts}/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
